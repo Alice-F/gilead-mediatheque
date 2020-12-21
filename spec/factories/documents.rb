a4e1558961821d329c131ebc1618b9d00a05846d
Alice Fabre <alice.fabre@hotmail.fr>
2020-12-21 11:21:12 +0100

    add document model

---
+++ spec/factories/documents.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :document do
+    title { "MyString" }
+    language { "MyString" }
+    usage { "MyText" }
+    validation_at { "2020-12-21 11:20:31" }
+    format { "MyString" }
+  end
+end
