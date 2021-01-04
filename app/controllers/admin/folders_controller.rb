6445bf5009723b46179069330fa5a0b29fb08342
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-04 12:40:12 +0100

    add actions to admin::folders controller

--- app/controllers/admin/folders_controller.rb
+++ app/controllers/admin/folders_controller.rb
@@ -1,2 +1,22 @@
 class Admin::FoldersController < ApplicationController
+  def new
+    @folder = Folder.new
+    authorize([:admin, @folder])
+  end
+
+  def create
+    @folder = Folder.new(folder_params)
+    authorize([:admin, @folder])
+    if @folder.save
+      redirect_to documents_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:name)
+  end
 end
